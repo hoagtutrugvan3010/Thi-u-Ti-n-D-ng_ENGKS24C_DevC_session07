092f99b15634374512375263cc77c946491a26dc
hoagtutrugvan3010 <thieutiendung3010@gmail.com>
2024-11-28 13:46:25 +0700

    Update and rename ss7bai2.cpp to ss7bai2.c

--- ss7bai2.c
+++ ss7bai2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    // Khai báo m?ng s? nguyên có 5 ph?n t?
+    // Khai bÃ¡o m?ng s? nguyÃªn cÃ³ 5 ph?n t?
     int array[5];
 
-    // Yêu c?u ngu?i dùng nh?p t?ng ph?n t?
+    // YÃªu c?u ngu?i dÃ¹ng nh?p t?ng ph?n t?
     printf("Nhap 5 phan tu cho mang:\n");
     for (int i = 0; i < 5; i++) {
         printf("Nhap phan tu thu %d: ", i + 1);
